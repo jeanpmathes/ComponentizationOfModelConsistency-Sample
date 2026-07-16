8fad418d4774732098688a365eaf8c2bbfecbc79
Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>
2026-07-16 18:54:39 +0200

    Finalize all infrastructure around view-based CPS
    Signed-off-by: Jean Patrick Mathes <jeanpatrick.mathes@gmx.de>

--- vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/ComponentizedConsistencyExampleTest.java
+++ vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/ComponentizedConsistencyExampleTest.java
@@ -1,12 +1,16 @@
 package tools.vitruv.methodologisttemplate.vsum;
 
-import mir.reactions.modelView2ModelView2.ModelView2ModelView2ChangePropagationSpecification;
+import mir.reactions.model2Model2.Model2Model2ChangePropagationSpecification;
+import mir.reactions.model2ModelView2.Model2ModelView2ChangePropagationSpecification;
+import mir.reactions.modelView2Model2.ModelView2Model2ChangePropagationSpecification;
 import neojoin.viewtypes.model2_identity.Model2IdentityViewType;
 import neojoin.viewtypes.model_identity.ModelIdentityViewType;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import tools.vitruv.change.propagation.ChangePropagationMode;
+import tools.vitruv.change.propagation.ChangePropagationSpecification;
 import tools.vitruv.change.testutils.TestUserInteraction;
 import tools.vitruv.compmodelcons.change.ViewChangePropagationSpecificationAdapterFactory;
 import tools.vitruv.framework.views.View;
@@ -22,11 +26,39 @@ import java.util.List;
 import java.util.Optional;
 
 public class ComponentizedConsistencyExampleTest extends AbstractTest {
-    @Test
-    void insertComponent(@TempDir Path tempDir) throws IOException {
-        VirtualModel vsum = createVirtualModel(tempDir);
+    @TempDir
+    private Path projectPath;
 
-        addSystem(vsum, tempDir);
+    private static List<ChangePropagationSpecification> getChangePropagationSpecifications(Configuration configuration) {
+        return switch (configuration) {
+            case NO_VIEW_USAGE -> ViewChangePropagationSpecificationAdapterFactory.INSTANCE.createRemote(
+                    Optional.empty(),
+                    new Model2Model2ChangePropagationSpecification(),
+                    Optional.empty());
+            case VIEW_AS_SOURCE -> ViewChangePropagationSpecificationAdapterFactory.INSTANCE.createRemote(
+                    Optional.of(new ModelIdentityViewType()),
+                    new ModelView2Model2ChangePropagationSpecification(),
+                    Optional.empty()
+            );
+            case VIEW_AS_TARGET -> ViewChangePropagationSpecificationAdapterFactory.INSTANCE.createRemote(
+                    Optional.empty(),
+                    new Model2ModelView2ChangePropagationSpecification(),
+                    Optional.of(new Model2IdentityViewType())
+            );
+            case VIEW_AS_SOURCE_AND_TARGET -> ViewChangePropagationSpecificationAdapterFactory.INSTANCE.createRemote(
+                    Optional.of(new ModelIdentityViewType()),
+                    new Model2ModelView2ChangePropagationSpecification(),
+                    Optional.of(new Model2IdentityViewType())
+            );
+        };
+    }
+
+    @ParameterizedTest
+    @EnumSource(Configuration.class)
+    void insertComponent(Configuration configuration) throws IOException {
+        VirtualModel vsum = createVirtualModel(configuration);
+
+        addSystem(vsum, projectPath);
         addComponent(vsum);
 
         Assertions.assertTrue(assertView(getDefaultView(vsum, List.of(System.class, Root.class)), (View v) -> {
@@ -37,16 +69,20 @@ public class ComponentizedConsistencyExampleTest extends AbstractTest {
         }));
     }
 
-    private InternalVirtualModel createVirtualModel(Path projectPath) throws IOException {
+    private InternalVirtualModel createVirtualModel(Configuration configuration) throws IOException {
         InternalVirtualModel model = new VirtualModelBuilder()
                 .withStorageFolder(projectPath)
                 .withUserInteractorForResultProvider(new TestUserInteraction.ResultProvider(new TestUserInteraction()))
-                .withChangePropagationSpecifications(ViewChangePropagationSpecificationAdapterFactory.INSTANCE.createRemote(
-                        Optional.of(new ModelIdentityViewType()),
-                        new ModelView2ModelView2ChangePropagationSpecification(),
-                        Optional.of(new Model2IdentityViewType())))
+                .withChangePropagationSpecifications(getChangePropagationSpecifications(configuration))
                 .buildAndInitialize();
         model.setChangePropagationMode(ChangePropagationMode.TRANSITIVE_CYCLIC);
         return model;
+    }
+
+    enum Configuration {
+        NO_VIEW_USAGE,
+        VIEW_AS_SOURCE,
+        VIEW_AS_TARGET,
+        VIEW_AS_SOURCE_AND_TARGET
     }
 }
